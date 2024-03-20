e87ed9c39e93dfd62b15e2655d43bd8070ea192a
Nate Hermes <natedog55902@yahoo.com>
2024-03-20 16:21:30 -0500

    Update working.py

--- working.py
+++ working.py
@@ -19,15 +19,47 @@ def print_board(bo):
     print ("\n")
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - -")
+            # print("- - - - - - - - - - - - -")
+            print("- - - - - - - - - - -")
         for j in range(len(bo[0])):
-            if j % 3 == 0:
+            # if j % 3 == 0:
+            if j % 3 == 0 and j != 0:
                 print("| ",end="")
 
             if j == 8:
-                print(str(bo[i][j]) + " |", end="\n")
+                # print(str(bo[i][j]) + " |", end="\n")
+                print(bo[i][j], end="\n")
             else:
                 print(str(bo[i][j]) + " ", end="")
     print ("\n")
 
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range (len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j) # (row, column) format
+
+def valid(bo, num, pos):
+    # check row
+    for i in range(len(bo)):
+        if bo[pos[0]][i] == num and pos[i] != i:
+            return False
+
+    # check column
+    for i in range(len(bo)):
+        if bo[i][pos[0]] == num and pos[0] != i:
+            return False
+
+    # check square
+    box_x = pos[1] // 3 #0, 1, or 2
+    box_y = pos[0] // 3 #0, 1, or 2
+
+    for i in range(box_y * 3, box_y * 3 + 3): #0 to 2, 3 to 5, or 6 to 8 (range is non-inclusive)
+        for j in range(box_x * 3, box_x * 3 + 3): #0 to 2, 3 to 5, or 6 to 8 (range is non-inclusive)
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+    
+    return True
+
 print_board(board)
+print (find_empty(board))
