026c1aa48c0b0cd613414207edf5329178e6c080
Nate Hermes <natedog55902@yahoo.com>
2024-04-03 14:44:12 -0500

    randomize and solve random board from pdf

--- working.py
+++ working.py
@@ -20,8 +20,9 @@ board = [
         [1, 2, 0, 0, 0, 7, 4, 0, 0],
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
-
 solutions = []
+boards = []
+path = os.path.join(os.getcwd(), "boards", "60_Sudokus_Pattern_Easy.pdf")
 
 def solve(bo,randomize=False):
     count = 0
@@ -51,7 +52,7 @@ def solve(bo,randomize=False):
 
 
 def print_board(bo):
-    # print ("\n")
+    print ("\n")
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
             # print("- - - - - - - - - - - - -")
@@ -108,36 +109,85 @@ def randomize_solvable_board(bo):
     print_board(bo)
 
 def randomize_board(bo):
-    # pick random number of movements for each step
-    num_moves = random.randint(0,50)
     # swap rows in block
-    # rows = random.sample(range(3),2)
-    # temp = bo[rows[0]]
-    # bo[rows[0]] = bo[rows[1]]
-    # bo[rows[1]] = temp
+    moves = random.randint(5,50)
+    for _ in range(moves):
+        rows = random.sample(range(3),2)
+        # print(rows)
+        temp = bo[rows[0]]
+        bo[rows[0]] = bo[rows[1]]
+        bo[rows[1]] = temp
 
     # swap rows of blocks
-    blocks = random.sample(range(2),2)
-    # blocks.sort(reverse=True)
-    # print(blocks)
-    for i in range(3):
-        swap1 = 3*blocks[0] + i
-        swap2 = 3*blocks[1] + i
-        temp = bo[swap1]
-        bo[swap1] = bo[swap2]
-        bo[swap2] = temp
+    moves = random.randint(5,50)
+    for _ in range(moves):
+        blocks = random.sample(range(3),2)
+        # print(blocks)
+        for i in range(3):
+            swap1 = 3*blocks[0] + i
+            swap2 = 3*blocks[1] + i
+            temp = bo[swap1]
+            bo[swap1] = bo[swap2]
+            bo[swap2] = temp
 
     # swap columns in block
+    temp_board = transform_board(bo)
+    moves = random.randint(5,50)
+    for _ in range(moves):
+        columns = random.sample(range(3),2)
+        # print(columns)
+        temp = temp_board[columns[0]]
+        temp_board[columns[0]] = temp_board[columns[1]]
+        temp_board[columns[1]] = temp
+    temp_board = transform_board(temp_board)
+    bo = temp_board
+
     # swap columns of blocks
+    temp_board = transform_board(bo)
+    moves = random.randint(5,50)
+    for _ in range(moves):
+        blocks = random.sample(range(3),2)
+        # print(blocks)
+        for i in range(3):
+            swap1 = 3*blocks[0] + i
+            swap2 = 3*blocks[1] + i
+            temp = temp_board[swap1]
+            temp_board[swap1] = temp_board[swap2]
+            temp_board[swap2] = temp
+    temp_board = transform_board(temp_board)
+    bo = temp_board
 
     # swap pairs of numbers
+    moves = random.randint(5,50)
+    for _ in range(moves):
+        pair = random.sample(range(1,10),2) # pick 2 numbers 1 through 9 (not blank spaces)
+        # print(pair)
+        for i in range(len(bo)):
+            for j in range(len(bo[0])):
+                if bo[i][j] == pair[0]:
+                    bo[i][j] = pair[1]
+                elif bo[i][j] == pair[1]:
+                    bo[i][j] = pair[0]
+
+    return bo
 
 def import_board():
     pass
 
+def transform_board(bo):
+    board2 = copy.deepcopy(bo)
+    for i in range(len(board2)):
+        for j in range(len(board2[0])):
+            board2[i][j] = 0
+    for i in range(len(board2)):
+        for j in range(len(board2[0])):
+            board2[i][j] = bo[j][i]
+
+    return board2
+
+
 # print_board(board)
 # print (find_empty(board))
-# num_list = numpy.array(range(1,10))
 # num_list = numpy.array(range(1,10))
 # print(num_list)
 # for i in num_list:
@@ -148,40 +198,37 @@ def import_board():
 #     print(i)
 # print(num_list)
 # solve(board)
-# randomize_board(board)
+
+# print_board(board)
+# board = randomize_board(board)
+# print_board(board)
+# solve(board)
 # print_board(board)
 
-board2 = copy.deepcopy(board)
-for i in range(len(board2)):
-    for j in range(len(board2[0])):
-        board2[i][j] = 0
-for i in range(len(board)):
-    for j in range(len(board[0])):
-        board2[i][j] = board[j][i]
+# for _ in range(10000):
+#     board2 = copy.deepcopy(board)
+#     board2 = randomize_board(board2)
+#     solve(board2)
+
+with pdfplumber.open(path) as f:
+    for page in f.pages:
+        # print(len(page.extract_tables()))
+        for table in page.extract_tables():
+            for i in range(len(table)):
+                for j in range(len(table[0])):
+                    if table[i][j] == '':
+                        table[i][j] = 0
+                    else:
+                        table[i][j] = int(table[i][j])
+            boards.append(table)
+random_board = random.choice(boards)
+print_board(random_board)
+random_board = randomize_board(random_board)
+print_board(random_board)
+solve(board)
 print_board(board)
-print_board(board2)
 
-# solve(board,randomize=False)
-# print_board(board)
-# print(len(solutions))
-# solves = 1
-# for k in range(solves):
-#     randomize_solvable_board(board)
-# print_board(board)
 
-path = os.path.join(os.getcwd(), "boards", "60_Sudokus_Pattern_Easy.pdf")
-
-# with pdfplumber.open(path) as f:
-#     for page in f.pages:
-#         # print(len(page.extract_tables()))
-#         for table in page.extract_tables():
-#             for i in range(len(table)):
-#                 for j in range(len(table[0])):
-#                     if table[i][j] == '':
-#                         table[i][j] = 0
-#                     else:
-#                         table[i][j] = int(table[i][j])
-#             print_board(table)
 
     #loop through all pages (f.pages)
     #loop through all tables ([0+i:0+i+9])
